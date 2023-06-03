1c0a2b0a00b033224f0fb27b19628ff28a2d22f3
Morozov Kirill <reddocwork@gmail.com>
2023-06-03 12:27:57 +0300

    Theme templates

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,14 +6,14 @@ import { useTheme } from "./providers/ThemeProvider"
 import AboutPage from "pages/AboutPage/ui/AboutPage"
 import { MainPage } from "pages/MainPage"
 import { AppRouter } from "./providers/routes"
+import { NavBar } from "widgets/NavBar"
 
 const App = () => {
   const {theme, toggleTheme} = useTheme()
   return (
     <div className={classNames('app', {}, [theme])}>
+      <NavBar />
       <button onClick={toggleTheme}>Toggle theme</button>
-      <Link to='/'>Главная</Link>
-      <Link to='/about'>О сайте</Link>
       <AppRouter />
     </div>
   )
